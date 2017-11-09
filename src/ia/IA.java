11730a4768a2e133420b08e7943a4de279a95688
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2017-11-09 09:18:59 +0100

    Subida de Marcos

--- src/ia/IA.java
+++ src/ia/IA.java
@@ -15,7 +15,9 @@ public class IA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+Muestra obx = new Muestra();
+System.out.println("Saludos"+obx.muestraNombre());
     }
     
 }
