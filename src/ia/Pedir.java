3d86f987a72c8fcfab5bc2c6e7f86f30775b017c
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-11-09 09:55:15 +0100

    Finalizado proyecto IA Muestra 2.0

--- src/ia/Pedir.java
+++ src/ia/Pedir.java
@@ -13,7 +13,16 @@ public class Pedir {
     Scanner sc=new Scanner(System.in);
     public String pedirNombre(){
         System.out.println("Como se llama usted?");
-        String nombre=sc.nextLine();
+        String nombre=sc.nextLine(); //Se guarda el nombre
         return nombre;
+    }
+    public String pedirApellidos(){
+        System.out.println("Cual es su primer apellido?");
+        String apellido1=sc.nextLine(); //Se pide y guarda el primer apellido
+        System.out.println("Y el segundo?");
+        String apellido2=sc.nextLine(); //Se pide y guarda el segundo apellido
+        String apellidos=apellido1+" "+apellido2; //Se guardan los dos apellidos en una sola variable
+        return apellidos;
+
     }
 }
